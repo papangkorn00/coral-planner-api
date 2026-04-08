d33c8d3f52746e9f5ee97d53601304e6447f80fd
papangkorn00 <papangkorn.poomm@gmail.com>
2026-04-08 22:52:21 +0700

    api init

---
+++ src/main/java/com/example/coralPlanner/CoralPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.coralPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoralPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoralPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coralPlanner/CoralPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coralPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoralPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
